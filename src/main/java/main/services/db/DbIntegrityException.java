46e7487195b48adabc2f40f28d7385fb843095c4
Joao Pedro <joaopdesousa12@gmail.com>
2023-09-21 00:26:55 -0300

    Classe DB

---
+++ src/main/java/main/services/db/DbIntegrityException.java
@@ -0,0 +1,9 @@
+package main.services.db;
+
+public class DbIntegrityException extends RuntimeException{
+    public static final long serialVersionUID = 1L;
+
+    public DbIntegrityException(String msg){
+        super(msg);
+    }
+}
